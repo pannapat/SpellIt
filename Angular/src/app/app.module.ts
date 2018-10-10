8943868fc940117c6a1264395d470136c882f7dc
Pannapat Chanpaisaeng <ch.pannapat@gmail.com>
2018-10-10 14:47:32 -0500

    Try adding angular-material form component to language-list page

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -1,15 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { LanguageListComponent } from './language-list/language-list.component';
-import { ParadigmListComponent } from './paradigm-list/paradigm-list.component';
-import { LanguageAddComponent } from './language-add/language-add.component';
-import { ParadigmEditComponent } from './paradigm-edit/paradigm-edit.component';
-import { ParadigmAddComponent } from './paradigm-add/paradigm-add.component';
-import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { SidebarComponent } from "./sidebar/sidebar.component";
+import { LanguageListComponent } from "./language-list/language-list.component";
+import { ParadigmListComponent } from "./paradigm-list/paradigm-list.component";
+import { LanguageAddComponent } from "./language-add/language-add.component";
+import { ParadigmEditComponent } from "./paradigm-edit/paradigm-edit.component";
+import { ParadigmAddComponent } from "./paradigm-add/paradigm-add.component";
+import { HttpClientModule } from "@angular/common/http";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatButtonModule, MatCheckboxModule, MatFormFieldModule, MatOptionModule, MatSelectModule, MatFormFieldControl, MatInputModule } from "@angular/material";
 
 @NgModule({
   declarations: [
@@ -24,9 +26,17 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatInputModule
   ],
+  exports: [MatButtonModule, MatCheckboxModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
