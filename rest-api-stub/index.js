7c1cdc8758fa6f58cec088e615cc3a8deef5ec21
Pannapat Chanpaisaeng <ch.pannapat@gmail.com>
2018-10-10 15:45:33 -0500

    Add API stub for http://localhost:8080/language-list

---
+++ rest-api-stub/index.js
@@ -0,0 +1,53 @@
+var http = require('http');
+var express = require('express');
+// var PORT = 9090;
+var PORT = process.env.PORT || 8080;
+var cors = require('cors');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+
+const app = express();
+// app.use(express.static('public'));
+app.use(cors({
+    credentials: true,
+    origin: true
+}));
+// app.use(bodyParser.json()); // support json encoded bodies
+// app.use(bodyParser.urlencoded({
+    // extended: true
+// }));
+// support encoded bodies
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.header('Access-Control-Allow-Credentials', true);
+
+    next();
+});
+
+app.get('/language-list', function (req, res, next) {
+    let response = [{
+            language_id: 'en_US',
+            language_name: 'English'
+        },
+        {
+            language_id: 'it_IT',
+            language_name: 'Italian'
+        },
+        {
+            language_id: 'ga_IE',
+            language_name: 'Irish'
+        },
+        {
+            language_id: 'th_TH',
+            language_name: 'Thai'
+        }
+    ];
+    res.send(response);
+    // res.send(JSON.stringify("hello world"));
+    // res.json({
+    //     msg: "hello"
+    // });
+});
+
+app.listen(PORT, () => console.log('RESTful API Stub app listening on port ' + PORT + '!'));
