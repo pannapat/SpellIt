fee78e182cf760307359ec56ab885b2c3097c4b8
Pannapat Chanpaisaeng <ch.pannapat@gmail.com>
2018-10-10 15:55:21 -0500

    Change DataService.getParadigmList to call API stub

--- Angular/src/app/data.service.ts
+++ Angular/src/app/data.service.ts
@@ -8,12 +8,12 @@ export class DataService {
   constructor(private http: HttpClient) {}
 
   getLanguageList() {
-    return this.http.get('http://localhost:8080/language-list');
+    return this.http.post('http://localhost:8080/language-list', {});
     // return this.http.get('https://jsonplaceholder.typicode.com/users');
     // return this.http.get('./app/mock-language-list.json');
   }
 
   getParadigmList() {
-  	return this.http.get('https://jsonplaceholder.typicode.com/users');
+    return this.http.post('http://localhost:8080/paradigm-list', {});
   }
 }
