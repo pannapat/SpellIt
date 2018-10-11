32e1d2d377f224b25cb2b62f40ffc2f6b23f521f
Pannapat Chanpaisaeng <ch.pannapat@gmail.com>
2018-10-10 22:03:36 -0500

    Add some data to rest-spi-stub

--- rest-api-stub/index.js
+++ rest-api-stub/index.js
@@ -14,7 +14,7 @@ app.use(cors({
 }));
 // app.use(bodyParser.json()); // support json encoded bodies
 // app.use(bodyParser.urlencoded({
-    // extended: true
+// extended: true
 // }));
 // support encoded bodies
 app.use(function (req, res, next) {
@@ -26,66 +26,148 @@ app.use(function (req, res, next) {
 });
 
 app.all('/language-list', function (req, res, next) {
-    let response = [{
-            language_id: 'en_US',
-            language_name: 'English'
-        },
-        {
-            language_id: 'it_IT',
-            language_name: 'Italian'
-        },
-        {
-            language_id: 'ga_IE',
-            language_name: 'Irish'
-        },
-        {
-            language_id: 'th_TH',
-            language_name: 'Thai'
-        }
-    ];
+    let response = LANGUGAE_LIST;
     res.send(response);
 });
 
 app.all('/paradigm-list', function (req, res, next) {
-    res.send([
-        {
-            paradigm_name: 'Verb'
-        },
-        {
-            paradigm_name: 'Noun'
-        },
-        {
-            paradigm_name: 'Verb ar'
-        },
-        {
-            paradigm_name: 'Verb xy'
-        }
-    ]);
-});
+    res.send(PARADIGM_LIST);
+})
 
 app.all('/paradigm', function (req, res, next) {
-    let response =
-        {
-            paradigm_name: 'Verb',
-            slots: [
-               "root",
-               "Present",
-               "Past",
-               "Participle"
-            ],
-            words: [{
-              "root": "walk",
-              "Present": "walks",
-              "Past": "walked",
-              "Participle": "walked"
-            }, {
-              "root": "jump",
-              "Present": "jumps",
-              "Past": "jumped",
-              "Participle": "jumped"
-            }]
-        }
-    res.send(response);
-});
+    const paradigm_id = req.get('paradigm_id');
+    let paradigm = {};
+
+    if (paradigm_id === 'verb' ||
+        paradigm_id === 'verb-ar' ||
+        paradigm_id === 'verb-xy') {
+        paradigm = PARADIGM_HASH['verb'];
+    } else if (paradigm_id === 'irregular-verb') {
+        paradigm = PARADIGM_HASH['irregular-verb'];
+    } else if (paradigm_id === 'noun') {
+        paradigm = PARADIGM_HASH['noun'];
+    }
+
+    res.send(pradigm);
+})
+
+app.all('/add-paradigm', function (req, res, next) {
+    res.send('ok');
+})
 
 app.listen(PORT, () => console.log('RESTful API Stub app listening on port ' + PORT + '!'));
+
+var LANGUGAE_LIST = [{
+        language_id: 'en_US',
+        language_name: 'English'
+    },
+    {
+        language_id: 'it_IT',
+        language_name: 'Italian'
+    },
+    {
+        language_id: 'ga_IE',
+        language_name: 'Irish'
+    },
+    {
+        language_id: 'th_TH',
+        language_name: 'Thai'
+    }
+];
+// var PARADIGM_LIST = [{
+//         paradigm_id: 'verb',
+//         paradigm_name: 'Verb'
+//     },
+//     {
+//         paradigm_id: 'irregular-verb',
+//         paradigm_name: 'Irregular Verb'
+//     },
+//     {
+//         paradigm_id: 'noun',
+//         paradigm_name: 'Noun'
+//     },
+//     {
+//         paradigm_id: 'verb-ar',
+//         paradigm_name: 'Verb ar'
+//     },
+//     {
+//         paradigm_id: 'verb-xy',
+//         paradigm_name: 'Verb xy'
+//     }
+// ];
+var PARADIGM_LIST = [
+    'Verb',
+    'Irregular Verb',
+    'Noun',
+    'Verb ar',
+    'Verb xy,'
+];
+var PARADIGM_HASH = {
+    'verb': {
+        paradigm_name: 'Verb',
+        slots: [
+            "root",
+            "Present",
+            "Past",
+            "Participle"
+        ],
+        words: [{
+            "root": "walk",
+            "Present": "walks",
+            "Past": "walked",
+            "Participle": "walked"
+        }, {
+            "root": "jump",
+            "Present": "jumps",
+            "Past": "jumped",
+            "Participle": "jumped"
+        }]
+    },
+    'irregular-verb': {
+        paradigm_name: 'Irregular Verb',
+        slots: [
+            "root",
+            "Present",
+            "Past",
+            "Participle"
+        ],
+        words: [{
+            "root": "go",
+            "Present": "goes",
+            "Past": "went",
+            "Participle": "gone"
+        }, {
+            "root": "take",
+            "Present": "takes",
+            "Past": "took",
+            "Participle": "taken"
+        }]
+    },
+    'noun': {
+        paradigm_name: 'Noun',
+        slots: [
+            "root",
+            "singular",
+            "plural"
+        ],
+        words: [{
+            "root": "fox",
+            "singular": "fox",
+            "plural": "foxes"
+        }]
+    }
+};
+
+let SAMPLE_REQUEST_ADD_LANGUAGE = {
+    language_id: 'eng_US',
+    language_name: 'English'
+}
+
+let SAMPLE_REQUEST_ADD_PARADIGM = {
+    paradigm_name: 'Noun',
+    slots: [
+        "root",
+        "singular",
+        "plural"
+    ]
+}
