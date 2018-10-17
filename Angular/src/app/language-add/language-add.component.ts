2789a8c0984ff2fcf51c9382a7118dcf6565a17a
Jyckle <jycklepasquesi@gmail.com>
2018-10-17 12:53:21 -0500

    Fixed up the whole setup to work with the backend API calls

--- Angular/src/app/language-add/language-add.component.ts
+++ Angular/src/app/language-add/language-add.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-language-add',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./language-add.component.scss']
 })
 export class LanguageAddComponent implements OnInit {
+  languageName: string;
+  status: Object;
 
-  constructor() { }
+  constructor(private data: DataService) { }
 
   ngOnInit() {
   }
+
+  addLanguage(){
+  	this.data.addLanguage(this.languageName).subscribe(
+      data => this.status = data
+    );
+  }
+  
 
 }
