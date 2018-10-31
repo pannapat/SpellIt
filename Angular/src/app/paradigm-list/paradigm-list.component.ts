01ca40c835a966c7b7ffd5a0e95106f53a56e4c2
Jyckle <jycklepasquesi@gmail.com>
2018-10-31 11:18:10 -0500

    basic download functionality is created

--- Angular/src/app/paradigm-list/paradigm-list.component.ts
+++ Angular/src/app/paradigm-list/paradigm-list.component.ts
@@ -39,7 +39,38 @@ export class ParadigmListComponent implements OnInit {
   }
 
   exportAffixFile() {
-    this.data.getAffix(this.language_name).subscribe();
+    alert("Download may take a few seconds")
+    this.data.getAffix(this.language_name).subscribe(data => {
+      var fileContents= data;
+      this.saveTextAsFile(fileContents, this.language_name + ".aff");
+      });  
+  }
+
+  saveTextAsFile (data, filename){
+    if(!data) {
+            console.error('No data to save')
+            return;
+        }
+
+        if(!filename) filename = 'affix.aff'
+
+        var blob = new Blob([data], {type: 'text/plain'}),
+            e    = document.createEvent('MouseEvents'),
+            a    = document.createElement('a')
+
+  if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+      window.navigator.msSaveOrOpenBlob(blob, filename);
+  }
+  else{
+      var e = document.createEvent('MouseEvents'),
+          a = document.createElement('a');
+
+      a.download = filename;
+      a.href = window.URL.createObjectURL(blob);
+      a.dataset.downloadurl = ['text/plain', a.download, a.href].join(':');
+      e.initEvent('click', false, false);
+      a.dispatchEvent(e);
+  }
   }
 
 }
