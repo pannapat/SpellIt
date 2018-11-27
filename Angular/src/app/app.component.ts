026c703140c78e2a861c82f59f1c1abc46535d42
Pannapat Chanpaisaeng <ch.pannapat@gmail.com>
2018-11-27 01:37:27 -0600

    Add Thai translation file, th.po file

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -19,5 +19,7 @@ export class AppComponent {
 
     // the lang to use, if the lang isn't available, it will use the current loader to get them
     translate.use('en');
+    // translate.use('ga');
+    // translate.use('th');
   }
 }
