19ffab39be2289a33a092275b05c3c0c02ea7d3c
Jyckle <jycklepasquesi@gmail.com>
2018-12-04 14:44:30 -0600

    minor change to resubmit travis test

--- test_sample.py
+++ test_sample.py
@@ -44,8 +44,7 @@ def getAffixes(language_list):
 				word_list_json = raw_words.json()
 				word_list = word_list_json["word_data"]
 				for key, value in word_list.items():
-					print key
-					print value
+					print key, value
 					words_file.write(value)
 
 		words_file.close()
